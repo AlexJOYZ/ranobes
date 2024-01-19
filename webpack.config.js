3a7de440a38cb444ff2c0e88d26163b5b1ed3f81
AlexJOYZ <zte19062015@gmail.com>
2024-01-19 17:38:14 +0800

    Complete settings Webpack for pet project

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,130 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const glob = require('glob');
+// import { generateHTMLPage } from "./src/modules/calc";
 
-const mode = process.env.NODE_ENV || "development";
-const devMode = mode === "development";
-const target = devMode ? "web" : "browserslist";
-const devtool = devMode ? "source-map" : undefined;
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+const pageNames = ['index.html', 'main.html'];
+function getMpa() {
+  const htmlwebpackplugins = [];
+  pageNames.forEach((pageName) => {
+    htmlwebpackplugins.push(
+      new HtmlWebpackPlugin({
+        Title: `${pageName} page`,
+        template: path.resolve(__dirname, 'src', 'pages', `${pageName}`),
+        filename: `${pageName}`,
+        MiniFy: true, // Generate the compressed HTML code.
+      }),
+    );
+  });
 
+  return htmlwebpackplugins;
+}
+
+const htmlwebpackplugins = getMpa(pageNames);
 module.exports = {
-    mode,
-    target,
-    devtool,
-    entry: path.resolve(__dirname, "src", "js", "app.js"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-        filename: "[name].[contenthash].js",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "pages", "index.html"),
-        }),
-        // new HtmlWebpackPlugin({
-        //     // template: path.resolve(__dirname, "src", "pages", "main.html"),
-        // }),
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: path.resolve(__dirname, 'src', 'js', 'app.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    ...htmlwebpackplugins,
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        generator: {
+          filename: 'pages/[name][ext]',
+        },
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
             },
-            {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
+          },
+          'sass-loader',
         ],
-    },
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: 'defaults',
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset/resource',
+        use: {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+            },
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.65, 0.9],
+              speed: 4,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75,
+            },
+          },
+        },
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+    ],
+  },
 };
